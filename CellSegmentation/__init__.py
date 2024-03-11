e711c64a235f6bdb65d6e858793e86f40cb69d88
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-03-11 13:32:30 +0530

    Setup

---
+++ CellSegmentation/__init__.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+import sys
+from from_root import from_root
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(from_root(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    format="[%(asctime)s] - %(name)s - %(levelname)s - %(module)s file - LineNum:%(lineno)d   - %(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger=logging.getLogger("cellSegmentation")
