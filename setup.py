1f6d41fb0c4a964b94375c3b7cf1709de6db032d
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-03-11 13:40:09 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'SunilKumar',
+    author_email= 'ugargolsunilkumar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
