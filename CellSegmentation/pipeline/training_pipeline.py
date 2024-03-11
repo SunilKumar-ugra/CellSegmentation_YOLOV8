8fcbfdfb46f1dceae85700c4b7bf918b56d328f0
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-03-11 19:11:28 +0530

    DATA VALIDATION

--- CellSegmentation/pipeline/training_pipeline.py
+++ CellSegmentation/pipeline/training_pipeline.py
@@ -2,18 +2,21 @@ import sys
 from CellSegmentation.logger import logging
 from CellSegmentation.exception import AppException
 from CellSegmentation.components.data_ingestion import DataIngestion
+from CellSegmentation.components.data_validation import DataValidation
 
 from CellSegmentation.entity import (DataIngestionConfig,
+                                     DataValidationConfig
                                                  )
 
 from CellSegmentation.entity.artifacts_entity import (DataIngestionArtifact,
+                                                      DataValidationArtifact
                                                     )
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        
+        self.data_validation_config = DataValidationConfig()
         
         
     def start_data_ingestion(self)-> DataIngestionArtifact:
@@ -38,11 +41,40 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys)    
         
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+
+     
+     
+     
         
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
         
         except Exception as e:
