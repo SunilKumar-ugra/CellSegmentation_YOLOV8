f8f34d344a2aa76d22bca37c49997ff532142571
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-03-14 23:53:16 +0530

    model trainer

--- CellSegmentation/pipeline/training_pipeline.py
+++ CellSegmentation/pipeline/training_pipeline.py
@@ -3,13 +3,15 @@ from CellSegmentation.logger import logging
 from CellSegmentation.exception import AppException
 from CellSegmentation.components.data_ingestion import DataIngestion
 from CellSegmentation.components.data_validation import DataValidation
-
+from CellSegmentation.components.model_trainer import ModelTrainer
 from CellSegmentation.entity import (DataIngestionConfig,
-                                     DataValidationConfig
+                                     DataValidationConfig,
+                                     ModelTrainerConfig
                                                  )
 
 from CellSegmentation.entity.artifacts_entity import (DataIngestionArtifact,
-                                                      DataValidationArtifact
+                                                      DataValidationArtifact,
+                                                      ModelTrainerArtifact
                                                     )
 
 
@@ -17,7 +19,8 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        
+        self.model_trainer_config = ModelTrainerConfig()
+
         
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try: 
@@ -66,7 +69,17 @@ class TrainPipeline:
             raise AppException(e, sys) from e
 
      
-     
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys) 
      
         
     def run_pipeline(self) -> None:
@@ -75,6 +88,11 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            
+            else:
+                raise Exception("Your data is not in correct format")
 
         
         except Exception as e:
